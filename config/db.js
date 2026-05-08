441b6923bae8597c1d7ed42adf5f0f5a9dd1d211
Rayan <sdtrrs05@gmail.com>
2026-05-08 16:48:48 +0100

    Fix railway database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mysite",
-  password: "5555",
-  port: 5432,
+  connectionString:
+    process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
