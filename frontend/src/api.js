139d7674760742a710b800bde98bfe927c54ad26
Rayan <sdtrrs05@gmail.com>
2026-05-12 19:17:38 +0100

    fix auth token

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,35 @@ const api = axios.create({
 
   baseURL:
     "https://vokko-d4tm.onrender.com"
+
 });
+
+/* ---------------- AUTH TOKEN ---------------- */
+
+api.interceptors.request.use(
+
+  (config) => {
+
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+
+      config.headers.Authorization =
+        `Bearer ${token}`;
+
+    }
+
+    return config;
+
+  },
+
+  (error) => {
+
+    return Promise.reject(error);
+
+  }
+
+);
 
 export default api;
